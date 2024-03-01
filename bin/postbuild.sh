6d4072f224fe79da292468a48d12d1473d41a92e
frontline <github@frontline-jp.tokyo>
2024-03-01 10:55:52 +0900

    amplify hosting

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -5,8 +5,10 @@ rm -rf ./.amplify-hosting
 mkdir -p ./.amplify-hosting/compute
 
 cp -r ./build/server ./.amplify-hosting/compute/default
-cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 
 cp -r ./build/client ./.amplify-hosting/static
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
+
+npm ci --omit=dev
+cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
