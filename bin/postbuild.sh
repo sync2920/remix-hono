1cfeee9b60058f679675ec134ceaacbe198db8c6
frontline <github@frontline-jp.tokyo>
2024-03-01 19:44:04 +0900

    amplify hosting test

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -11,9 +11,6 @@ cp -r ./build/client ./.amplify-hosting/static
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
 
 npm ci --omit=dev
-rm -rf ./node_modules/@cloudflare
-rm -rf ./node_modules/vite/node_modules/esbuild
-rm -rf ./node_modules/@vanilla-extract
-rm -rf ./node_modules/vite-node
-rm -rf ./node_modules/prettier
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
+
+cp .env ./.amplify-hosting/compute/default/.env
