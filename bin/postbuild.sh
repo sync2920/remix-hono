a9b41e9503f1e0272bc42bd4ef95ed65a0ac0b0a
frontline <github@frontline-jp.tokyo>
2024-03-02 09:03:03 +0900

    amplify hosting test

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -1,16 +1,16 @@
-##!/bin/bash
-#
-#rm -rf ./.amplify-hosting
-#
-#mkdir -p ./.amplify-hosting/compute
-#
-#cp -r ./build/server ./.amplify-hosting/compute/default
-#
-#cp -r ./build/client ./.amplify-hosting/static
-#
-#cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
-#
-#npm ci --omit=dev
-#cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
-#
-#cp .env ./.amplify-hosting/compute/default/.env
+#!/bin/bash
+
+rm -rf ./.amplify-hosting
+
+mkdir -p ./.amplify-hosting/compute
+
+cp -r ./build/server ./.amplify-hosting/compute/default
+
+cp -r ./build/client ./.amplify-hosting/static
+
+cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
+
+npm ci --omit=dev
+cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
+
+cp .env ./.amplify-hosting/compute/default/.env
